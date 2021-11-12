49e73d0ead84a8d013d3bb6041462aad8552348c
Cahllagerfeld <Cahllagerfeld>
2021-11-12 16:54:51 +0000

    feat: init discordmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { DiscordModule } from '@discord-nestjs/core';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DiscordConfigService } from './environment/discord-config.service';
+import { ThreadModule } from './thread/thread.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DiscordModule.forRootAsync({ useClass: DiscordConfigService }),
+    ThreadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
